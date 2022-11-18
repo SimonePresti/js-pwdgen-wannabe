3071676f3713d817d43e37dff37a5c720606ed26
SimonePresti <prestisimone96@gmail.com>
2022-11-18 15:09:02 +0100

    create const prompt

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const inserisciNome = prompt ('inserisci il nome');
+
+const inserisciCognome = prompt ('inserisci il cognome');
+
+const inserisciColorePreferito = prompt ('inserisci il colore preferito');
+
+console.log (inserisciNome, inserisciCognome, inserisciColorePreferito );
